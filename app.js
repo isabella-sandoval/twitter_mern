926b8da6316473ba3dc85815c7771a6b7724c9c3
Isabella <isabella.sandoval95@gmail.com>
2020-10-26 04:07:13 -0600

    twitter

--- app.js
+++ app.js
@@ -4,20 +4,35 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
 const bodyParser = require('body-parser');
-
-app.get("/", (req, res) => res.send("Hello World"));
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
-
-mongoose
-.connect(db, { useNewUrlParser: true })
-.then(() => console.log("Connected to MongoDB successfully"))
-.catch(err => console.log(err));
+const passport = require('passport');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: "user",
+        email: "user@test.com",
+        password: "password"
+    })
+    user.save()
+    res.send("MERN Twitter");
+})
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
