dde0e7bd2ae4d3f6c3c0c65cd5f67fe20d056ad5
Isabella <isabella.sandoval95@gmail.com>
2020-10-26 04:32:09 -0600

    heroku

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const tweets = require("./routes/api/tweets");
 const User = require('./models/User');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,6 +15,12 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
